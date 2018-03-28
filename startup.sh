7c34262a8df9822a43c099fd0ae61ddd5332c796
Chris Dent <chris.dent@gmail.com>
2018-03-28 20:51:57 +0100

    Make syncing the DB optional
    This makes building a container for use with devstack (and other
    situations where the db is already created and synchronized)
    simple:
    
       docker run [...] -e DB_SYNC=False -e DB_STRING=some_db_uri [...]

--- startup.sh
+++ startup.sh
@@ -2,12 +2,13 @@
 # way startup is done in the Dockerfile.
 
 DB_STRING=${DB_STRING:-sqlite:////cats.db}
+DB_SYNC=${DB_SYNC:-True}
 
 # Do substitutions in the template.
 sed -e "s,{DB_CONNECTION},$DB_STRING," < /etc/nova/nova.conf.tmp > /etc/nova/nova.conf
 
-# establish the database
-python3 /sync.py --config-file /etc/nova/nova.conf
+# establish the database, only if we've been asked to do so.
+[ "$DB_SYNC" = "True" ] && python3 /sync.py --config-file /etc/nova/nova.conf
 
 # run the web server
 /usr/sbin/uwsgi --ini /placement-uwsgi.ini

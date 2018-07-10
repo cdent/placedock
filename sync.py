ae89c5e4a2b5360bb8f90694c4303bcbdb8f27aa
Chris Dent <chris.dent@gmail.com>
2018-07-10 21:51:28 +0100

    Use only placement migrations in sync.py
    That way we avoid creating tables that won't be used.

--- sync.py
+++ sync.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import sys
 
@@ -10,20 +11,33 @@ from nova.api.openstack.placement import db_api
 from nova import conf
 
 
+# This needs to be updated to as new placement
+# migrations are added.
+PLACEMENT_MIGRATIONS = [
+    '016_resource_providers.py',
+    '026_add_resource_classes.py',
+    '029_placement_aggregates.py',
+    '041_resource_provider_traits.py',
+    '043_placement_consumers.py',
+    '044_placement_add_projects_users.py',
+    '051_nested_resource_providers.py',
+    '059_add_consumer_generation.py',
+]
+
+
 def _migration():
     # hack to the path of nova/db/sqlalchemy/migrate_repo/
     rel_path = os.path.join('..', 'db', 'sqlalchemy', 'api_migrations',
                             'migrate_repo')
-    sys.stderr.write('rel_path %s\n' % rel_path)
     path = os.path.join(os.path.abspath(os.path.dirname(conf.__file__)),
                         rel_path)
-    sys.stderr.write('path %s\n' % path)
-    # adjust the keypairs migration otherwise the entire world
-    # gets imported because it imports nova.objects.keypair for a single
-    # string constant and that result in every object being imported.
-    bad_file = os.path.join(path, 'versions', '014_keypairs.py')
-    with open(bad_file, 'w') as stopper:
-        stopper.write('def upgrade(x): pass\n')
+    # only use those migrations which actually do something for placement
+    migration_dir_glob = os.path.join(path, 'versions', '*.py')
+    for migration in glob.iglob(migration_dir_glob):
+        if os.path.basename(migration) not in PLACEMENT_MIGRATIONS:
+            with open(migration, 'w') as stopper:
+                stopper.write('def upgrade(x): pass\n')
+
     repository = Repository(path)
     placement_engine = db_api.get_placement_engine()
     try:

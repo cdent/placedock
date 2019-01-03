2386a7f61743d6dcb00e8dbe4bc78bc9b7058430
Chris Dent <chris.dent@gmail.com>
2019-01-03 12:27:54 +0000

    Remove DB_CLEAN from startup.sh
    It's no longer used. With alembic in place in placement, it's
    simple and fine to let sync do the work.

--- startup.sh
+++ startup.sh
@@ -5,7 +5,6 @@ set -a
 
 OS_PLACEMENT_DATABASE__CONNECTION=${OS_PLACEMENT_DATABASE__CONNECTION:-sqlite:////cats.db}
 DB_SYNC=${DB_SYNC:-False}
-DB_CLEAN=${DB_CLEAN:-False}
 OS_API__AUTH_STRATEGY=${OS_API__AUTH_STRATEGY:?OS_API__AUTH_STRATEGY required}
 
 # establish the database, only if we've been asked to do so.

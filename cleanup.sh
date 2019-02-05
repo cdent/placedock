9727fb3358b35aa73b9a3cbc864587f28ebbb818
Chris Dent <chris.dent@gmail.com>
2019-02-05 18:56:31 +0000

    Clean out unused stuff now that there's a helm chart
    Also move startup.sh to shared, since it is copied
    into the container.
    
    Documentation will be updated to reflect changes to
    use helm in a followup.

--- cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -x
-
-eval `minikube docker-env`
-kubectl delete hpa placement-lba
-kubectl delete service placement-deployment
-kubectl delete deployment placement-deployment
-docker rmi placetest -f
-
